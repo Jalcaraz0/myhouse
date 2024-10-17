3c4c596084a236af2ea6cb8221f3b75588312a83
Juan Alcaraz <juanalcaraz@MacBook-Pro-de-Juan.local>
2024-10-16 18:21:52 -0600

    Feature | Project setup | Juan Alcaraz

---
+++ app/src/main/java/com/example/myhouse/MainActivitySystemView.kt
@@ -0,0 +1,13 @@
+package com.example.myhouse
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivitySystemView: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+}
